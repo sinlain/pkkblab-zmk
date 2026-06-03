1eeb23fa2281e81589f0dc550960a79d5db239c5
sinlain <sdb4929@gmail.com>
2026-06-04 02:35:41 +0900

    Update behavior_cheonjiin.c

--- src/behaviors/behavior_cheonjiin.c
+++ src/behaviors/behavior_cheonjiin.c
@@ -26,6 +26,10 @@
 #include <dt-bindings/zmk/hid_usage_pages.h>
 #include <dt-bindings/zmk/keys.h>
 
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+#endif
+
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
@@ -33,6 +37,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #define TAP_DELAY_MS 20
 #define CJI_REPEAT_TERM_MS 1800
 #define CJI_ACTION_QUEUE_SIZE 64
+#define CJI_VOWEL_SEQ_MAX 5
+#define CJI_VOWEL_OUT_MAX 2
 
 enum cji_kind {
     CJI_KIND_NONE = 0,
@@ -47,6 +53,11 @@ struct cji_state {
     enum cji_kind last_kind;
     int64_t last_tap_time;
     bool pending_dot;
+
+    uint32_t vowel_seq[CJI_VOWEL_SEQ_MAX];
+    uint8_t vowel_len;
+    bool emitted_vowel;
+    uint8_t emitted_vowel_out_len;
 };
 
 static struct cji_state state = {
@@ -55,6 +66,9 @@ static struct cji_state state = {
     .last_kind = CJI_KIND_NONE,
     .last_tap_time = 0,
     .pending_dot = false,
+    .vowel_len = 0,
+    .emitted_vowel = false,
+    .emitted_vowel_out_len = 0,
 };
 
 enum cji_action_type {
@@ -225,6 +239,9 @@ static void reset_state(void) {
     state.last_kind = CJI_KIND_NONE;
     state.last_tap_time = 0;
     state.pending_dot = false;
+    state.vowel_len = 0;
+    state.emitted_vowel = false;
+    state.emitted_vowel_out_len = 0;
 }
 
 static bool is_within_term(void) {
@@ -365,102 +382,271 @@ static int handle_consonant(uint32_t token) {
     state.last_token = token;
     state.last_kind = CJI_KIND_CONSONANT;
     state.pending_dot = false;
+    state.vowel_len = 0;
+    state.emitted_vowel = false;
+    state.emitted_vowel_out_len = 0;
+    stamp_tap_time();
+
+    return 0;
+}
+
+struct cji_output_key {
+    uint32_t keycode;
+    bool shifted;
+};
+
+struct cji_vowel_map {
+    uint32_t seq[CJI_VOWEL_SEQ_MAX];
+    uint8_t seq_len;
+    struct cji_output_key out[CJI_VOWEL_OUT_MAX];
+    uint8_t out_len;
+};
+
+/*
+ * Cheonjiin vowel sequences used by this firmware:
+ *
+ * Basic:
+ * ㅏ = ㅣ + ㆍ
+ * ㅓ = ㆍ + ㅣ
+ * ㅗ = ㅡ + ㆍ
+ * ㅜ = ㆍ + ㅡ
+ * ㅡ = ㅡ
+ * ㅣ = ㅣ
+ *
+ * Extended:
+ * ㅑ = ㅣ + ㆍ + ㆍ
+ * ㅕ = ㆍ + ㆍ + ㅣ
+ * ㅛ = ㅡ + ㆍ + ㆍ
+ * ㅠ = ㆍ + ㆍ + ㅡ
+ * ㅐ = ㅣ + ㆍ + ㅣ
+ * ㅒ = ㅣ + ㆍ + ㆍ + ㅣ
+ * ㅔ = ㆍ + ㅣ + ㅣ
+ * ㅖ = ㆍ + ㆍ + ㅣ + ㅣ
+ * ㅘ = ㅡ + ㆍ + ㅣ + ㆍ
+ * ㅙ = ㅡ + ㆍ + ㅣ + ㆍ + ㅣ
+ * ㅚ = ㅡ + ㆍ + ㅣ
+ * ㅝ = ㆍ + ㅡ + ㆍ + ㅣ
+ * ㅞ = ㆍ + ㅡ + ㆍ + ㅣ + ㅣ
+ * ㅟ = ㆍ + ㅡ + ㅣ
+ * ㅢ = ㅡ + ㅣ
+ */
+static const struct cji_vowel_map vowel_maps[] = {
+    { .seq = {CJI_I},                                  .seq_len = 1, .out = {{L, false}},                 .out_len = 1 }, /* ㅣ */
+    { .seq = {CJI_EU},                                 .seq_len = 1, .out = {{M, false}},                 .out_len = 1 }, /* ㅡ */
+    { .seq = {CJI_I, CJI_DOT},                         .seq_len = 2, .out = {{K, false}},                 .out_len = 1 }, /* ㅏ */
+    { .seq = {CJI_DOT, CJI_I},                         .seq_len = 2, .out = {{J, false}},                 .out_len = 1 }, /* ㅓ */
+    { .seq = {CJI_EU, CJI_DOT},                        .seq_len = 2, .out = {{H, false}},                 .out_len = 1 }, /* ㅗ */
+    { .seq = {CJI_DOT, CJI_EU},                        .seq_len = 2, .out = {{N, false}},                 .out_len = 1 }, /* ㅜ */
+
+    { .seq = {CJI_I, CJI_DOT, CJI_DOT},                .seq_len = 3, .out = {{I, false}},                 .out_len = 1 }, /* ㅑ */
+    { .seq = {CJI_DOT, CJI_DOT, CJI_I},                .seq_len = 3, .out = {{U, false}},                 .out_len = 1 }, /* ㅕ */
+    { .seq = {CJI_EU, CJI_DOT, CJI_DOT},               .seq_len = 3, .out = {{Y, false}},                 .out_len = 1 }, /* ㅛ */
+    { .seq = {CJI_DOT, CJI_DOT, CJI_EU},               .seq_len = 3, .out = {{B, false}},                 .out_len = 1 }, /* ㅠ */
+
+    { .seq = {CJI_I, CJI_DOT, CJI_I},                  .seq_len = 3, .out = {{O, false}},                 .out_len = 1 }, /* ㅐ */
+    { .seq = {CJI_I, CJI_DOT, CJI_DOT, CJI_I},         .seq_len = 4, .out = {{O, true}},                  .out_len = 1 }, /* ㅒ */
+    { .seq = {CJI_DOT, CJI_I, CJI_I},                  .seq_len = 3, .out = {{P, false}},                 .out_len = 1 }, /* ㅔ */
+    { .seq = {CJI_DOT, CJI_DOT, CJI_I, CJI_I},         .seq_len = 4, .out = {{P, true}},                  .out_len = 1 }, /* ㅖ */
+
+    { .seq = {CJI_EU, CJI_DOT, CJI_I, CJI_DOT},        .seq_len = 4, .out = {{H, false}, {K, false}},     .out_len = 2 }, /* ㅘ */
+    { .seq = {CJI_EU, CJI_DOT, CJI_I, CJI_DOT, CJI_I}, .seq_len = 5, .out = {{H, false}, {O, false}},     .out_len = 2 }, /* ㅙ */
+    { .seq = {CJI_EU, CJI_DOT, CJI_I},                 .seq_len = 3, .out = {{H, false}, {L, false}},     .out_len = 2 }, /* ㅚ */
+
+    { .seq = {CJI_DOT, CJI_EU, CJI_DOT, CJI_I},        .seq_len = 4, .out = {{N, false}, {J, false}},     .out_len = 2 }, /* ㅝ */
+    { .seq = {CJI_DOT, CJI_EU, CJI_DOT, CJI_I, CJI_I}, .seq_len = 5, .out = {{N, false}, {P, false}},     .out_len = 2 }, /* ㅞ */
+    { .seq = {CJI_DOT, CJI_EU, CJI_I},                 .seq_len = 3, .out = {{N, false}, {L, false}},     .out_len = 2 }, /* ㅟ */
+
+    { .seq = {CJI_EU, CJI_I},                          .seq_len = 2, .out = {{M, false}, {L, false}},     .out_len = 2 }, /* ㅢ */
+};
+
+static bool vowel_seq_matches(const struct cji_vowel_map *map) {
+    if (map->seq_len != state.vowel_len) {
+        return false;
+    }
+
+    for (uint8_t i = 0; i < state.vowel_len; i++) {
+        if (map->seq[i] != state.vowel_seq[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static bool vowel_seq_is_prefix(const struct cji_vowel_map *map) {
+    if (state.vowel_len > map->seq_len) {
+        return false;
+    }
+
+    for (uint8_t i = 0; i < state.vowel_len; i++) {
+        if (map->seq[i] != state.vowel_seq[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static const struct cji_vowel_map *find_vowel_map(void) {
+    for (uint8_t i = 0; i < ARRAY_SIZE(vowel_maps); i++) {
+        if (vowel_seq_matches(&vowel_maps[i])) {
+            return &vowel_maps[i];
+        }
+    }
+
+    return NULL;
+}
+
+static bool has_vowel_prefix(void) {
+    for (uint8_t i = 0; i < ARRAY_SIZE(vowel_maps); i++) {
+        if (vowel_seq_is_prefix(&vowel_maps[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static int emit_vowel_map(const struct cji_vowel_map *map) {
+    int err;
+
+    for (uint8_t i = 0; i < map->out_len; i++) {
+        if (map->out[i].shifted) {
+            err = tap_shifted_key(map->out[i].keycode);
+        } else {
+            err = tap_key(map->out[i].keycode);
+        }
+
+        if (err < 0) {
+            return err;
+        }
+    }
+
+    return 0;
+}
+
+static int erase_emitted_vowel(void) {
+    int err;
+    uint8_t erase_count = state.emitted_vowel_out_len;
+
+    if (erase_count == 0) {
+        erase_count = 1;
+    }
+
+    for (uint8_t i = 0; i < erase_count; i++) {
+        err = tap_backspace();
+        if (err < 0) {
+            return err;
+        }
+    }
+
+    return 0;
+}
+
+static void reset_vowel_sequence(void) {
+    state.vowel_len = 0;
+    state.emitted_vowel = false;
+    state.emitted_vowel_out_len = 0;
+    state.pending_dot = false;
+}
+
+static void append_vowel_token(uint32_t token) {
+    if (state.vowel_len < CJI_VOWEL_SEQ_MAX) {
+        state.vowel_seq[state.vowel_len++] = token;
+    }
+}
+
+static int start_new_vowel_sequence(uint32_t token) {
+    const struct cji_vowel_map *map;
+    int err;
+
+    reset_vowel_sequence();
+    append_vowel_token(token);
+
+    map = find_vowel_map();
+    if (map != NULL) {
+        err = emit_vowel_map(map);
+        if (err < 0) {
+            return err;
+        }
+
+        state.emitted_vowel = true;
+        state.emitted_vowel_out_len = map->out_len;
+        state.last_kind = CJI_KIND_VOWEL;
+    } else {
+        state.emitted_vowel = false;
+        state.emitted_vowel_out_len = 0;
+        state.last_kind = CJI_KIND_DOT;
+    }
+
+    state.last_token = token;
+    state.repeat_count = 0;
+    state.pending_dot = (map == NULL);
+    stamp_tap_time();
+
+    return 0;
+}
+
+static int handle_vowel_token(uint32_t token) {
+    int err;
+    const struct cji_vowel_map *map;
+
+    bool continuing =
+        (state.last_kind == CJI_KIND_VOWEL || state.last_kind == CJI_KIND_DOT) &&
+        is_within_term();
+
+    if (!continuing) {
+        return start_new_vowel_sequence(token);
+    }
+
+    append_vowel_token(token);
+
+    map = find_vowel_map();
+
+    if (map == NULL && !has_vowel_prefix()) {
+        return start_new_vowel_sequence(token);
+    }
+
+    if (map != NULL) {
+        if (state.emitted_vowel) {
+            err = erase_emitted_vowel();
+            if (err < 0) {
+                return err;
+            }
+        }
+
+        err = emit_vowel_map(map);
+        if (err < 0) {
+            return err;
+        }
+
+        state.emitted_vowel = true;
+        state.emitted_vowel_out_len = map->out_len;
+        state.last_kind = CJI_KIND_VOWEL;
+    } else {
+        state.emitted_vowel = false;
+        state.emitted_vowel_out_len = 0;
+        state.last_kind = CJI_KIND_DOT;
+    }
+
+    state.last_token = token;
+    state.repeat_count = 0;
+    state.pending_dot = (map == NULL);
     stamp_tap_time();
 
     return 0;
 }
 
 static int handle_vowel_i(void) {
-    int err;
-
-    if (state.pending_dot &&
-        state.last_token == CJI_DOT &&
-        is_within_term()) {
-        /* ㆍ + ㅣ = ㅓ */
-        err = tap_key(J);
-        reset_state();
-        return err;
-    }
-
-    err = tap_key(L); /* ㅣ */
-    if (err < 0) {
-        return err;
-    }
-
-    state.last_token = CJI_I;
-    state.last_kind = CJI_KIND_VOWEL;
-    state.pending_dot = false;
-    state.repeat_count = 0;
-    stamp_tap_time();
-
-    return 0;
-}
-
-static int handle_vowel_eu(void) {
-    int err;
-
-    if (state.pending_dot &&
-        state.last_token == CJI_DOT &&
-        is_within_term()) {
-        /* ㆍ + ㅡ = ㅜ */
-        err = tap_key(N);
-        reset_state();
-        return err;
-    }
-
-    err = tap_key(M); /* ㅡ */
-    if (err < 0) {
-        return err;
-    }
-
-    state.last_token = CJI_EU;
-    state.last_kind = CJI_KIND_VOWEL;
-    state.pending_dot = false;
-    state.repeat_count = 0;
-    stamp_tap_time();
-
-    return 0;
+    return handle_vowel_token(CJI_I);
 }
 
 static int handle_vowel_dot(void) {
-    int err;
+    return handle_vowel_token(CJI_DOT);
+}
 
-    if (state.last_token == CJI_I &&
-        state.last_kind == CJI_KIND_VOWEL &&
-        is_within_term()) {
-        /* ㅣ + ㆍ = ㅏ */
-        err = tap_backspace();
-        if (err < 0) {
-            return err;
-        }
-
-        err = tap_key(K);
-        reset_state();
-        return err;
-    }
-
-    if (state.last_token == CJI_EU &&
-        state.last_kind == CJI_KIND_VOWEL &&
-        is_within_term()) {
-        /* ㅡ + ㆍ = ㅗ */
-        err = tap_backspace();
-        if (err < 0) {
-            return err;
-        }
-
-        err = tap_key(H);
-        reset_state();
-        return err;
-    }
-
-    /* Standalone ㆍ has no direct 2-beolsik key. */
-    state.last_token = CJI_DOT;
-    state.last_kind = CJI_KIND_DOT;
-    state.pending_dot = true;
-    state.repeat_count = 0;
-    stamp_tap_time();
-
-    return 0;
+static int handle_vowel_eu(void) {
+    return handle_vowel_token(CJI_EU);
 }
 
 static int handle_token(uint32_t token) {
